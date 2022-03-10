719ef9a35b8dfe3abb961ccea062405adb62ec46
AlbertoBiga <alberto.biga10@gmail.com>
2022-03-10 20:06:08 +0100

    manca solo generare numeri random

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,34 +1,42 @@
-//RECUPERO DATO DA INPUT
-
-let km = document.getElementById("kilometriSelezionati");
-let nominativo = document.getElementById("nomeCognome");
-let età = document.getElementById("fasciaEtàSelezionata");
-
 let genera = document.getElementById("genera");
+let biglietto = document.getElementById("biglietto");
 
-genera.addEventListener("click",
-  function(){
-    console.log(km.value);
-    console.log(nominativo.value)
-    console.log(età.value)
-  }
-);
+//creare l'evento al click
+genera.addEventListener("click",  
+  function() {
+    biglietto.classList.toggle("d-none");
+    let nominativo = document.getElementById("nomeCognome")    
+    let  km  = document.getElementById("kilometri")
+    let eta = document.getElementById("età")
+    document.getElementById("nomepasseggero").innerHTML = "<h5>" + nominativo.value + "</h5>"
+    document.getElementById("offerta").innerHTML = "<h5>" + eta.value + "</h5>"    
+    document.getElementById("carrozza").innerHTML = "<h5> 5 </h5>"
+    document.getElementById("codicecp").innerHTML = "<h5>"+ "10098" +"</h5>"
+
+    let prezzo = km.value * 0.21
+    let prezzoMinorenne = ( prezzo - ( prezzo * 0.2 ))
+    let prezzoMinorenneArrotondato = prezzoMinorenne.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+    console.log( 'prezzo minorenne  ' + prezzoMinorenneArrotondato)
+
+    let prezzoOver65 = ( prezzo - ( prezzo * 0.4 ))
+    let prezzoOver65Arrotondato = prezzoOver65.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+    console.log( 'prezzo over65  ' + prezzoOver65Arrotondato)
+
+    let prezzoStandard = prezzo
+    let prezzoStandardArrotondato = prezzoStandard.toFixed(2) //PREZZI ARROTONDATI 2 CIFRE DECIMALI
+    console.log('prezzo standard  ' + prezzoStandardArrotondato)
 
 
-//CALCOLO PREZZO
-// let prezzo = ( km * 0.21 )
-// console.log( prezzo)
-
-
-// SCONTO 20% MINORENNE SCONTO 40% OVER 65
-// let prezzoMinorenne = ( prezzo - ( prezzo * 0.2 ))
-// let prezzoMinorenneArrotondato = prezzoMinorenne.toFixed(2) 
-// console.log( 'prezzo minorenne  ' + prezzoMinorenneArrotondato)
-
-// let prezzoOver65 = ( prezzo - ( prezzo * 0.4 ))
-// let prezzoOver65Arrotondato = prezzoOver65.toFixed(2) 
-// console.log( 'prezzo over65  ' + prezzoOver65Arrotondato)
-
-// let prezzoStandard = prezzo
-// let prezzoStandardArrotondato = prezzoStandard.toFixed(2) 
-// console.log('prezzo standard  ' + prezzoStandardArrotondato)
+    //OUTPUT FINALE
+    if ( eta.value == 1) {
+        document.getElementById("costobiglietto").innerHTML ="<h5>" + prezzoMinorenneArrotondato + " euro" + "</h5>";
+        console.log ('prezzo definitivo  ' + prezzoMinorenneArrotondato)
+    } else if ( eta.value == 2 ) {
+        document.getElementById("costobiglietto").innerHTML = "<h5>" + prezzoOver65Arrotondato + " euro" + "</h5>";
+        console.log ('prezzo definitivo  ' + prezzoOver65Arrotondato)
+    } else {
+        document.getElementById("costobiglietto").innerHTML = "</h5>" + prezzoStandardArrotondato + " euro" + "</h5>";
+        console.log ('prezzo definitivo  ' + prezzoStandardArrotondato)
+    }
+        }
+    );
